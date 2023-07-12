4d31d11f6335092786deef1f8952d614749c48ff
kizitech <kizitoohani@gmail.com>
2023-07-12 19:11:35 +0100

    complete contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,13 +4,14 @@ function Contact() {
   return (
     <>
       <div className='contact'>
-        <section>
-          <form action='' method=''>
-            <input type="text" name="name" id="name" placeholder='Name' className='contact__input' />
-            <input type="email" name="email" id="email" placeholder='Email' className='contact__input' />
-            <textarea name="Message" id="Message" cols="65" rows="20" className='contact__textarea'></textarea>
-          </form>
-        </section>
+        <section className='contact-bg'></section>
+        
+        <form action='' method='' className='contact-form'>
+          <input type="text" name="name" id="name" placeholder='Name' className='contact__input' />
+          <input type="email" name="email" id="email" placeholder='Email' className='contact__input' />
+          <textarea name="Message" id="Message" cols="65" rows="20" placeholder='Message' className='contact__textarea'></textarea>
+          <button type="submit" className='contact__btn'>Send</button>
+        </form>
       </div>
     </>
   )
