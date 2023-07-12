52b9e99d835be441b5667df9d6fa9244c0b36645
kizitech <kizitoohani@gmail.com>
2023-07-12 09:20:27 +0100

    Tuesday begining

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -13,7 +13,8 @@ function Menu() {
 
         <section className="food-cards">
           <h4 className='food-card__heading'>Special Menu</h4>
-          {specialMenu.map((card1, i) => {
+          <div className='food-card__container'>
+            {specialMenu.map((card1, i) => {
             return (
               <div key={i} className="food-card">
                 <div className="food-card__image-card">
@@ -27,48 +28,55 @@ function Menu() {
                 </div>
               </div>
             );
-            
+
           })}
+          </div>
         </section>
 
         <section className="food-cards">
           <h4 className='food-card__heading'>Appetizers</h4>
-          {appetizers.map((card2, i) => {
-            return (
-              <div key={i} className="food-card">
-                <div className="food-card__image">
-                  <img src={card2.image} alt="Delicious Delicacy" />
-                </div>
+          
+          <div className='food-card__container'>
+            {appetizers.map((card2, i) => {
+              return (
+                <div key={i} className="food-card">
+                  <div className="food-card__image">
+                    <img src={card2.image} alt="Delicious Delicacy" />
+                  </div>
 
-                <div className="food-card__content">
-                  <h2 className="food-card__title">{card2.title}</h2>
-                  <h3 className="food-card__price">{card2.price}</h3>
-                  <button className="food-card__button">View Product</button>
+                  <div className="food-card__content">
+                    <h2 className="food-card__title">{card2.title}</h2>
+                    <h3 className="food-card__price">{card2.price}</h3>
+                    <button className="food-card__button">View Product</button>
+                  </div>
                 </div>
-              </div>
-            );
-            
-          })}
+              );
+
+            })}
+          </div>
         </section>
 
         <section className="food-cards">
           <h4 className='food-card__heading'>Vegan Desserts</h4>
-          {veganDesserts.map((card3, i) => {
-            return (
-              <div key={i} className="food-card">
-                <div className="food-card__image">
-                  <img src={card3.image} alt="Delicious Delicacy" />
-                </div>
+          
+          <div className='food-card__container'>
+            {veganDesserts.map((card3, i) => {
+              return (
+                <div key={i} className="food-card">
+                  <div className="food-card__image">
+                    <img src={card3.image} alt="Delicious Delicacy" />
+                  </div>
 
-                <div className="food-card__content">
-                  <h2 className="food-card__title">{card3.title}</h2>
-                  <h3 className="food-card__price">{card3.price}</h3>
-                  <button className="food-card__button">View Product</button>
+                  <div className="food-card__content">
+                    <h2 className="food-card__title">{card3.title}</h2>
+                    <h3 className="food-card__price">{card3.price}</h3>
+                    <button className="food-card__button">View Product</button>
+                  </div>
                 </div>
-              </div>
-            );
-            
-          })}
+              );
+
+            })}
+          </div>
         </section>
       </div>
     </>
