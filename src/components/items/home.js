daff36b9a88b5d0915c962da64a56fcb3ac130a2
kizitech <kizitoohani@gmail.com>
2023-07-13 15:06:01 +0100

    working on the home page

--- src/components/items/home.js
+++ src/components/items/home.js
@@ -3,35 +3,35 @@ const ourProduct = [
 
     {
         image: 'src/assets/images/home/Rectangle 4 (1).png',
-        title: 'Small Chops',
+        title: 'Waina',
     },
 
     {
         image: 'src/assets/images/home/Rectangle 5.png',
-        title: 'Nakia (Rice cake)',
+        title: 'Scented Poached Paw-paw',
     },
 
     {
         image: 'src/assets/images/home/Rectangle 6.png',
-        title: 'Gulisua',
+        title: 'Beans & Plantain Waffles',
 
     },
 
     {
         image: 'src/assets/images/home/Rectangle 7.png',
-        title: 'Tuwon Madara',
+        title: 'Chocolate malva pudding',
 
     },
 
     {
         image: 'src/assets/images/home/Rectangle 8.png',
-        title: 'Rouge Dessert',
+        title: 'Peanut Butter Cookies',
 
     },
 
     {
         image: 'src/assets/images/home/Rectangle 9.png',
-        title: 'Shuku Shuku',
+        title: 'Vanilla Pancake',
 
     },
 
@@ -45,13 +45,13 @@ const specialDessert = [
     
         {
             image: 'src/assets/images/home/Rectangle 11.png',
-            title: 'Peanut Butter Cookies',
+            title: 'Puff Puff',
     
         },
 
     {
         image: 'src/assets/images/home/Rectangle 10.png',
-        title: 'Plantain Chips',
+        title: 'Triffle',
 
     },
 
@@ -64,19 +64,19 @@ const popularCategories = [
 
     {
         image: 'src/assets/images/home/Rectangle 20.png',
-        title: 'Bean/Plantain Waffles',
+        title: 'Beju',
 
     },
 
     {
         image: 'src/assets/images/home/Rectangle 21.png',
-        title: 'Banana/Almond Butter Cake',
+        title: 'Hot cross buns',
 
     },
 
     {
         image: 'src/assets/images/home/Rectangle 22.png',
-        title: 'Plantain Chocolate Fudge Cake',
+        title: 'Shuku shuku',
 
     },
 ]
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,73 +5,122 @@ import { ourProduct, specialDessert, popularCategories } from '../components/ite
 function Home() {
   return (
     <>
-      <div>
-
-        <section>
-          <section className="food-cards">
-            <h4 className='food-card__heading'>Special Menu</h4>
-            <div className='food-card__container'>
-              {ourProduct.map((card1, i) => {
-                return (
-                  <div key={i} className="food-card">
-                    <div className="food-card__image-card">
-                      <img className="food-card__image" src={card1.image} alt="Delicious Delicacy" />
-                    </div>
-
-                    <div className="food-card__content">
-                      <h2 className="food-card__title">{card1.title}</h2>
-                    </div>
-                  </div>
-                );
-
-              })}
+      <main className="main" id="main">
+        <div className='hero-wrapper'>
+          <section className='hero'>
+            <div className="hero__image"></div>
+            <div className='hero__text-content'>
+              <h1 className='hero__text-content-heading'>Indulge In The Delicacies Of African Desserts</h1>
+              <p className='hero__text-content-description'>Sweeten Your Life With Our African Delicacies</p>
+              <button className='hero__text-content-btn' type="submit">Order Now</button>
             </div>
           </section>
-
-          <section className="food-cards">
-            <h4 className='food-card__heading'>Appetizers</h4>
-
-            <div className='food-card__container'>
-              {specialDessert.map((card2, i) => {
-                return (
-                  <div key={i} className="food-card">
-                    <div className="food-card__image">
-                      <img src={card2.image} alt="Delicious Delicacy" />
-                    </div>
-
-                    <div className="food-card__content">
-                      <h2 className="food-card__title">{card2.title}</h2>
-                    </div>
-                  </div>
-                );
-
-              })}
-            </div>
+          <section className='rune-info'>
+            <p>Rune, Established in 2019 is a family owned, African dessert company. we are known for flavorful, real taste products,
+              that conveys a sense of excitement and let you feel the taste of home.</p>
           </section>
 
-          <section className="food-cards">
-            <h4 className='food-card__heading'>Vegan Desserts</h4>
+          <div className="hero-detail">
+            <section className="products">
+              <div className="product-cards">
+                <h2 className="product-card__heading">Our Products</h2>
+                <div className="product-card__container">
+                  {ourProduct.map((card, i) => {
+                    return (
+                      <div key={i} className="product-card">
+                        <div className="product-card__image">
+                          <img className="product-card__image" src={card.image} alt="Delicious Delicacy" />
+                        </div>
 
-            <div className='food-card__container'>
-              {popularCategories.map((card3, i) => {
-                return (
-                  <div key={i} className="food-card">
-                    <div className="food-card__image">
-                      <img src={card3.image} alt="Delicious Delicacy" />
-                    </div>
+                        <div className="product-card__content">
+                          <h2 className="product-card__title">{card.title}</h2>
+                        </div>
+                      </div>
+                    );
 
-                    <div className="food-card__content">
-                      <h2 className="food-card__title">{card3.title}</h2>
-                    </div>
-                  </div>
-                );
+                  })}
+                </div>
+              </div>
 
-              })}
-            </div>
-          </section>
-        </section>
+              <div className="product-cards">
+                <h2 className="product-card__heading">Our Products</h2>
+                <div className="product-card__container">
+                  {specialDessert.map((card, i) => {
+                    return (
+                      <div key={i} className="product-card">
+                        <div className="product-card__image">
+                          <img className="product-card__image" src={card.image} alt="Delicious Delicacy" />
+                        </div>
 
-      </div>
+                        <div className="product-card__content">
+                          <h2 className="product-card__title">{card.title}</h2>
+                        </div>
+                      </div>
+                    );
+
+                  })}
+                </div>
+              </div>
+
+              <div className="product-cards">
+                <h2 className="product-card__heading">Our Products</h2>
+                <div className="product-card__container">
+                  {popularCategories.map((card, i) => {
+                    return (
+                      <div key={i} className="product-card">
+                        <div className="product-card__image">
+                          <img className="product-card__image" src={card.image} alt="Delicious Delicacy" />
+                        </div>
+
+                        <div className="product-card__content">
+                          <h2 className="product-card__title">{card.title}</h2>
+                        </div>
+                      </div>
+                    );
+
+                  })}
+                </div>
+              </div>
+            </section>
+
+            <section className="main-content">
+              <div className="main-content__text">
+                <h2 className="main-content__text-title">Place an order and get your dessert delivered to your doorstep.</h2>
+                <p className="main-content__text-description">Get in touch with us today and experience dessert done right!</p>
+                <button className='main-content__text-btn' type="submit">Order Now</button>
+              </div>
+              <div className="main-content__image">
+                <img src="src/assets/images/home/Rectangle 23.png" alt="A Lady enjoying herself" />
+              </div>
+            </section>
+
+            <section className="opening-hours">
+              <div className="opening-hours__image"></div>
+              <h1 className="opening-hours__title">Opening Hours</h1>
+              <div className="opening-hours__details">
+                <span className="opening-hours__day">
+                  <h4>Monday - Thursday</h4>
+                  <p>10AM - 9PM</p>
+                </span>
+                <span className="opening-hours__day">
+                  <h4>Friday</h4>
+                  <p>10AM - 12AM</p>
+                </span>
+                <span className="opening-hours__day">
+                  <h4>Saturday</h4>
+                  <p>10AM - 11PM</p>
+                </span>
+                <span className="opening-hours__day">
+                  <h4>Sunday</h4>
+                  <p>10AM - 7PM</p>
+                </span>
+              </div>
+            </section>
+
+          </div>
+
+        </div>
+      </main>
     </>
   )
 }
